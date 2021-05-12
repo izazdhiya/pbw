fd838d62b93d60b5532b0b8bcf4754de1cd8ea80
izazdhiya <59567289+izaz-id@users.noreply.github.com>
2021-05-12 19:52:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["login"])) {
+	header("Location: login.php");
+	exit;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Selamat Datang!</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-7 col-lg-5">
+					<div class="login-wrap p-4 p-md-5">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<span class="fa fa-user-o"></span>
+						</div>
+						<h3 class="text-center mb-4">My Account</h3>
+						<a href="logout.php" class="form-control btn btn-primary rounded submit px-3"> <span>Logout</span> </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
